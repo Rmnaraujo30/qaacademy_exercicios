563744859aaf54b06d9925100f8cd1f5f1199219
Rmnaraujo30 <ramon.araujo_san@hotmail.com>
2022-04-02 15:20:59 -0300

    exc 5 refatoração

--- src/easy/exercicio05.java
+++ src/easy/exercicio05.java
@@ -4,25 +4,22 @@ import javax.swing.JOptionPane;
 
 public class exercicio05 {
 
-    public static void main(String[] args) {
 
-        double numero1, numero2, numero3;
-        double soma, subtracao, multiplicacao, media;
-
-        numero1 = Double.parseDouble(JOptionPane.showInputDialog("digite um numero para calcular a soma"));
-        numero2 = Double.parseDouble(JOptionPane.showInputDialog("digite um numero para calcular a sutração"));
-        numero3 = Double.parseDouble(JOptionPane.showInputDialog("digite um numero para calcular a multiplicação"));
-        
-        soma = numero1 + numero2 + numero3;
-        subtracao = numero1 - numero2 - numero3;
-        multiplicacao = numero1 * numero2 * numero3;
-        media = numero1 + numero2 + numero3 / 3;
-
-        System.out.println(" A soma dos números digitado é " + soma);
-        System.out.println(" A subtração dos números digitado é " + subtracao);
-        System.out.println(" A multipicação dos números digitado é " + multiplicacao);
-        System.out.println(" A média dos números digitado é " + media);
+        public int calcularSoma(int numero1, int numero2, int numero3) {
+            int soma = numero1 + numero2 + numero3;
+            return soma;
+        }
+        public int calcularSubtracao(int numero1, int numero2, int numero3) {
+            return numero1 - numero2 - numero3;
+        }
+        public int calcularMultiplicacao(int numero1, int numero2, int numero3) {
+            return numero1 * numero2 * numero3;
+        }
+        public double calcularMedia(double numero1, double numero2, double numero3) {
+            return (numero1 + numero2 + numero3) / 3;
+        }
+    
 
     }
 
-}
+
